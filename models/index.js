cc342089523c479f4c683b753425e66e7fcfa4f8
David Williams <williamsdavida@johndeere.com>
2020-11-12 14:52:41 -0600

    dotenv error

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-dotenv.config();
+
 
 let sequelize;
 if (process.env.DATABASE_URL) {
