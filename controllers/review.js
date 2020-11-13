7ff6382a77b0fa02aea876fd6016a1cfa74d8b24
David Williams <williamsdavida@johndeere.com>
2020-11-13 16:48:34 -0600

    set up route to get all reviews

--- controllers/review.js
+++ controllers/review.js
@@ -4,15 +4,12 @@ const Trails = require('../models').trails;
 
 
 const getAllReviews = (req,res) => {
-    console.log("Made it to request")
     Review.findAll()
     .then(reviews => {
-        // res.status(200).send('OK')
-        res.send(reviews);
+        res.status(200).send(reviews);
     })
     .catch((err) => {
-        res.send(err)
-        res.send("Request, Failed")
+        res.status(500).send("Request, Failed")
     })
 }
 
